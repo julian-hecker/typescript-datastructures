de46433dac6dfa65a7c2bb060141fa30ee6927e1
Julian Hecker <julianheckerdev@gmail.com>
2021-04-22 07:09:20 -0400

    add test files for stack and queue

---
+++ test/Queue.test.ts
@@ -0,0 +1,14 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import Queue from '../src/Queue';
+
+describe('Queue', () => {
+    describe('Constructing', () => {});
+
+    describe('Adding Items', () => {});
+
+    describe('Removing Items', () => {});
+
+    describe('', () => {});
+});
---
+++ test/Stack.test.ts
@@ -0,0 +1,25 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import Stack from '../src/Stack';
+
+
+describe('Stack', () => {
+    describe('Constructing', () => {
+        
+    });
+
+    describe('Adding Items', () => {
+        
+    });
+
+    describe('Removing Items', () => {
+        
+    });
+    
+    describe('', () => {
+        
+    });
+    
+    
+});
